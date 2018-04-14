1ec0097105bba127e76c0a170a3eaeef9dcc7952
Eduardo <eduardo.rinaldi1998@gmail.com>
2018-04-14 17:11:15 +0200

    remove blt

---
+++ stringhe.asm
@@ -0,0 +1,33 @@
+.data
+str: .asciiz "Jamm bell ja"
+
+.text
+.globl main
+#a0 indirizzo stringa
+scorri_e_stampa:
+	li $t0, 0	#inizializzo la i
+	for:
+	add $s0, $a0, $t0	#calcolo indirizzo	
+	lb $s0, ($s0)	#s0<-str[i]
+	beqz $s0, fine_for	#if(s0 == 0) fine for
+	
+	subi $sp, $sp, 4
+	sw $a0, ($sp)
+	
+	move $a0, $s0
+	li $v0, 11
+	syscall
+	
+	lw $a0, ($sp)
+	addi $sp, $sp, 4
+	addi $t0, $t0, 1
+	j for
+	fine_for:
+	jr $ra
+main:
+	la $a0, str
+	jal scorri_e_stampa
+	
+	li $v0, 10
+	syscall
+
