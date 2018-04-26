379b7e61e9e3aaae1ec42d28b9c0aa983072c4aa
Eduardo <eduardo.rinaldi1998@gmail.com>
2018-04-26 20:26:55 +0200

    finished

--- Homework02/hw2.asm
+++ Homework02/hw2.asm
@@ -1,6 +1,10 @@
 .data
-grid: .byte ' ':64
+grid: .byte ' ':65
 coords: .space 6
+patta: .asciiz "PATTA\n"
+nero: .asciiz "STA VINCENDO IL NERO\n"
+bianco: .asciiz "STA VINCENDO IL BIANCO\n"
+error: .asciiz "MOSSA ERRATA\n"
 .text
 .globl main
 
@@ -29,6 +33,19 @@ coords: .space 6
 	syscall
 	.end_macro
 
+.macro getVal(%mat, %i,%j)
+	# mat = matrix address
+	# i = row index
+	# j = column index
+	li $v0, 0
+	#(8*i)+j
+	sll $t0, %i, 3
+	add $t0, $t0, %j
+	
+	lb $v0, %mat($t0)
+	
+	.end_macro
+
 .macro insertValReg(%mat, %i, %j, %val)
 	# mat = matrix address
 	# i = row index
@@ -36,10 +53,11 @@ coords: .space 6
 	# val = value to insert
 	
 	# (8*i)+j	
-	sll %i, %i, 3
-	add %i, %i, %j
+	sll $t4, %i, 3
+	add $t4, $t4, %j
 	
-	sb %val, %mat(%i)	# insert value in matAddress[i][j]
+	sb %val, %mat($t4)	# insert value in matAddress[i][j]
+	li $t4, 0
 	.end_macro
 
 .macro insertVal(%mat,%i,%j,%val)
@@ -165,6 +183,7 @@ coords: .space 6
 	printCharMat(%mat,7,7)
 	printChar('|')
 	printChar('\n')
+	printChar('\n')
 	.end_macro
 	
 
@@ -175,15 +194,141 @@ coords: .space 6
 	#insert N
 	insertVal(%mat,3,4,'N')
 	insertVal(%mat,4,3,'N')
+	insertVal(%mat,8,0,0)
 	.end_macro
 	
 	
+.macro inside(%x,%y)
+	li $v0, 0
+	bltz %x, endInside
+	bge %x, 8, endInside
+	bltz %y, endInside
+	bge %y, 8, endInside
+	li $v0, 1
+	endInside:
+	 
+	.end_macro
 	
-	
+
+.macro countNB(%g,%n,%b)
+	la $s5, %g
+	li %n, 0 #count n
+	li %b, 0 #count b
+	countp:
+	lb $t5, ($s5)
+	beqz $t5,  endCountp
+	bne $t5, 'N',checkB
+	addi %n, %n, 1
+	j noOne
+	checkB:
+	bne $t5, 'B', noOne
+	addi %b, %b, 1
+	j noOne
+	noOne:
+	addi $s5, $s5, 1
+	j countp
+	endCountp:
+	.end_macro
 
 #---------- END MACROS ----------
 
-		
+#---------- FUNCTIONS ----------
+
+# a0= x   a1= y   a2= player
+checkCoords:
+	getVal(grid,$a1,$a0)
+	bne $v0, ' ', endCheckCoords
+	
+	#for i=-1 to 1, for j=-1 to 1
+	#a3 = i    t1 = j
+	
+	li $a3, -1
+	li $t1, -1
+	ICicle:
+	beq $a3, 2, endCheckCoords
+	JCicle:
+	beq $t1, 2, endJCicle
+	
+	bnez $a3, continueCheck
+	bnez $t1, continueCheck
+	addi $t1, $t1, 1
+	
+	continueCheck:
+	#store on stack
+	subi $sp, $sp, 4
+	sw $ra, ($sp)
+	
+	jal checkRecursive
+	
+	#restore from stack
+	lw $ra, ($sp)
+	addi $sp, $sp, 4
+	addi $t1, $t1, 1
+	j JCicle
+	endJCicle:
+	li $t1, -1
+	addi $a3, $a3, 1
+	j ICicle
+	endCheckCoords:
+		jr $ra	
+# a0= x   a1= y   a2=p    a3= i   t1 = j
+checkRecursive:
+	add $t2, $a0, $t1	#t2 = x+j
+	add $t3, $a1, $a3	#t3 = y+i
+	
+	#if not inside(x+j,y+i) or g[y+i][x+j] == ' ': return False
+	inside($t2,$t3)		
+	beq $v0, 1, otherControlBeforeCheckRecursive
+	li $v1, 0
+	jr $ra
+	otherControlBeforeCheckRecursive:
+	getVal(grid,$t3,$t2)
+	bne $v0, ' ', otherControlCheckRecursive
+	li $v1, 0
+	jr $ra
+	otherControlCheckRecursive:
+	#if g[y+i][x+j] == p: return True
+	getVal(grid,$t3,$t2)
+	bne $v0, $a2, ControlCheckRecursive
+	li $v1, 1
+	jr $ra
+	ControlCheckRecursive:
+	
+	#store on stack
+	subi $sp, $sp, 20
+	sw $a0, ($sp)
+	sw $a1, 4($sp)
+	sw $t2, 8($sp)
+	sw $t3, 12($sp)
+	sw $ra, 16($sp)
+	
+	#new params
+	add $a0, $t2, $zero	#x+j
+	add $a1, $t3, $zero	#y+i
+	
+	jal checkRecursive
+	
+	#restore from stack
+	lw $a0, ($sp)
+	lw $a1, 4($sp)
+	lw $t2, 8($sp)
+	lw $t3, 12($sp)
+	lw $ra, 16($sp)
+	addi $sp, $sp, 20
+	
+	beqz $v1, endCheckRecursive
+	li $s2, 1			#flag = True
+	insertValReg(grid,$a1,$a0,$a2)	#g[y][x] = p
+	insertValReg(grid,$t3,$t2,$a2)	#g[y+i][x+j] = p
+	li $v1, 1
+	jr $ra
+	
+	endCheckRecursive:
+		li $v1, 0
+		jr $ra
+	
+
+	
 
 main:
 	
@@ -197,6 +342,9 @@ main:
 	startGame:
 	beq $s0, $zero, endGame
 	
+	#initialize flag
+	li $s2, 0
+	
 	# read string in input
 	la $a0, coords
 	li $a1, 6
@@ -207,25 +355,37 @@ main:
 	lb $t0, coords
 	beq $t0, 'S', endGame
 	
-	# j = coords[0] - 97
-	lb $t0, coords($zero)
-	subi $t0, $t0, 97
+	# x = coords[0] - 97
+	lb $s3, coords($zero)
+	subi $s3, $s3, 97
 	
-	# i = coords[1] - 48
-	lb $t1, coords($s0)
-	subi $t1, $t1, 48
+	# y = coords[1] - 48
+	lb $s4, coords($s0)
+	subi $s4, $s4, 49
+	
+
+	#insertValReg
+	add $a0, $s3, $zero
+	add $a1, $s4, $zero
+	add $a2, $s1, $zero
+	jal checkCoords
 	
 	# N || B ?
+	bne $s2, 1, printMossaNonValida
 	beq $s1, 'N', toWhite
 	li $s1, 'N'
-	j afterChange
+	j printMatrixx
 	toWhite:
 	li $s1, 'B'
 	
-	#insertValReg
-	afterChange:
-	insertValReg(grid,$t1,$t0,$s1)
+	printMatrixx:
 	printMatrix(grid)
+	j startGame
+	printMossaNonValida:
+	#printMatrix(grid)
+	la $a0, error
+	li $v0, 4
+	syscall
 	j startGame
 	
 	
@@ -233,6 +393,25 @@ main:
 	 
 	
 	endGame:
+	countNB(grid, $s6, $s7)
+	beq $s6, $s7, pattaGame
+	bgt $s6, $s7, nWin
+
+	#vince bianco
+	la $a0, bianco
+	li $v0, 4
+	syscall
+	j end
+	nWin:
+	la $a0, nero
+	li $v0, 4
+	syscall
+	j end
+	pattaGame:
+	la $a0, patta
+	li $v0, 4
+	syscall
+	end:
 	li $v0, 10
 	syscall
 	
--- Homework02/hw2Asm.py
+++ Homework02/hw2Asm.py
@@ -10,17 +10,22 @@ def changePlayer(p): return 'N' if player == 'B' else 'B'
 def inside(x,y): return 0<=x<8 and 0<=y<8
 
 
-def checkRecursive3(g,x,y,p,i,j,f=False):
-    if not inside(x+j,y+i) or g[y+i][x+j] == ' ': return False,f
-    if g[y+i][x+j] == p: return True,f
+def checkRecursive3(g,x,y,p,i,j,lnb):
+    if not inside(x+j,y+i) or g[y+i][x+j] == ' ': return False,lnb
+    if g[y+i][x+j] == p: return True,lnb
 
-    if checkRecursive3(g,x+j,y+i,p,i,j):
-        f = True
+    if checkRecursive3(g,x+j,y+i,p,i,j,lnb):
+        if p == 'N':
+            lnb[0]+=1
+            lnb[1]-=1
+        else:
+            lnb[0]-=1
+            lnb[1]+=1
         g[y][x] = p
         g[y+i][x+j] = p
         #checkCoords(g,x,y,p)
-        return True, f
-    return False, f
+        return True,lnb
+    return False,lnb
 
 
 
@@ -69,14 +74,13 @@ def checkRecursive2(g,x,y,p):
             g[y + dir1[i][0]][x + dir1[i][1]] = p
             checkRecursive(g, x + dir1[i][1], y + dir1[i][0], p)
 
-def checkCoords(g,x,y,p):
+def checkCoords(g,x,y,p,lnb):
     if g[y][x] == ' ':
         for i in range(-1, 2):
             for j in range(-1, 2):
                 if i == 0 and j == 0: continue
-                inutile, flag = checkRecursive3(g,x,y,p,i,j)
-                flag = True if flag else False
-                print(flag)
+                print(checkRecursive3(g,x,y,p,i,j,lnb))
+
 
 
 
@@ -89,7 +93,7 @@ matrix[4][3] = 'N'
 a = 0
 player = 'N'
 printGrid(matrix)
-
+lnb = [2,2]
 
 while True:
     print("Current player: "+player)
@@ -99,7 +103,7 @@ while True:
 
     # coords[0] = X , coords[1] = Y
     coords = (ord(coords[0])-97, int(coords[1])-1)
-    checkCoords(matrix, coords[0], coords[1], player)
+    checkCoords(matrix, coords[0], coords[1], player, lnb)
 
     printGrid(matrix)
     player = changePlayer(player)
